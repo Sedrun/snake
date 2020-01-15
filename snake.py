7d34dd4435a0d631345bbb2960308d7f031156cb
Sedrun <ili-dan@hotmail.fr>
2020-01-15 19:59:27 +0100

    Update snake.py

--- snake.py
+++ snake.py
@@ -20,19 +20,19 @@ image_pomme = pygame.transform.scale(image_pomme, (taille, taille))
 mange = pygame.mixer.Sound("sounds/eat.wav")
 pygame.mixer.music.load("sounds/song.wav")
 
+#BOUCLE PRINCIPALE
 continuer = 1
 fenetre_accueil = 1
 continuer_jeu = 0
 game_over = 0
-#BOUCLE PRINCIPALE
-#NEW
+
 while continuer:
     pygame.mixer.music.play()
     snake = Snake(image_snake, 9, 9)
     snake_accueil = Snake(image_snake, 0, 0)
     for i in range(5) : snake_accueil.addCarre()
     score = Score()
-#ENew
+
 
     pomme = Pomme(image_pomme, 30)
 
@@ -40,25 +40,23 @@ while continuer:
 
     direction = 'droite'
 
-#NEW
+
     #FENETRE D'ACCUEIL
     while fenetre_accueil:
-        pygame.time.Clock().tick(FPS)
+        pygame.time.Clock().tick(30)
         # Ecran accueil
         fenetre.blit(texte_accueil, texte_accueil_xy)
         fenetre.blit(texte_jouer, texte_jouer_xy)  # affichage de text_explicatif1
         fenetre.blit(texte_quitter, texte_quitter_xy)  # affichage de text_explicatif2
+        # Animation
+        snake_accueil.accueil()
+        fenetre.blit(image_snake, (snake_accueil.x, snake_accueil.y))
+        image_corps = pygame.image.load(corps).convert_alpha()
+        for i in range(1, snake_accueil.nb_carres):
+            image_corps = pygame.transform.scale(image_corps, (snake_accueil.carres[i].taille, snake_accueil.carres[i].taille))
+            fenetre.blit(image_corps, (snake_accueil.carres[i].x, snake_accueil.carres[i].y))
 
-        #TODO:animation a mettre dans la classe Snake
-        if snake_accueil.case_y == 0 and snake_accueil.case_x < 19:
-            snake_accueil.deplacer('droite')
-        if snake_accueil.case_x == 19 and snake_accueil.case_y < 19:
-            snake_accueil.deplacer('bas')
-        if snake_accueil.case_y == 19 and snake_accueil.case_x > 0:
-            snake_accueil.deplacer('gauche')
-        if snake_accueil.case_x == 0 and snake_accueil.case_y > 0:
-            snake_accueil.deplacer('haut')
-
+         # Interaction joueur
         for event in pygame.event.get():
             if event.type == QUIT:
                 fenetre_accueil = 0
@@ -72,19 +70,15 @@ while continuer:
                     continuer = 0
                     fenetre_accueil = 0
 
-        fenetre.blit(image_snake, (snake_accueil.x, snake_accueil.y))
-        image_corps = pygame.image.load(corps).convert_alpha()
-        for i in range(1, snake_accueil.nb_carres):
-            image_corps = pygame.transform.scale(image_corps, (snake_accueil.carres[i].taille, snake_accueil.carres[i].taille))
-            fenetre.blit(image_corps, (snake_accueil.carres[i].x, snake_accueil.carres[i].y))
+
 
         pygame.display.flip()  # permet un affichage dynamique dans cette fenêtre (on ne voit plus le serpent
         fenetre.blit(background, (0, 0))
-#ENEW
+
     #BOUCLE DE JEU
     while continuer_jeu:
         #limitation de vitesse de la boucle
-        pygame.time.Clock().tick(FPS)
+        pygame.time.Clock().tick(30)
         for event in pygame.event.get():
             #Si l'utilisateur quitte, on met la variable qui continue le jeu
             #ET la variable générale à 0 pour fermer la fenêtre
@@ -99,24 +93,18 @@ while continuer:
                 #Touches de déplacement de Donkey Kong
                 elif event.key == K_RIGHT and direction != 'gauche':
                     direction = 'droite'
-                    break; #permet d'enlever le demi-tour immédiat
                 elif event.key == K_LEFT and direction != 'droite':
                     direction = 'gauche'
-                    break;
                 elif event.key == K_UP and direction != 'bas':
                     direction = 'haut'
-                    break;
                 elif event.key == K_DOWN and direction != 'haut':
                     direction = 'bas'
-                    break;
-        snake.deplacer(direction)
+
         if snake.case_x == pomme.case_x and snake.case_y == pomme.case_y:
-            mange.play()
             pomme.changement()
             snake.addCarre()
-            # New
+            mange.play()
             score.ajoute()
-            # ENew
 
         for i in range(snake.nb_carres-1, 1, -1):
             # vérifie si il est mort
@@ -128,19 +116,25 @@ while continuer:
             continuer_jeu = 0
             game_over = 1
 
-        #Affichage aux nouvelles positions
+        # New
+        snake.deplacer(direction)
+        # Affichage aux nouvelles positions
         fenetre.blit(background, (0,0))
         fenetre.blit(image_snake, (snake.x, snake.y))
         image_corps = pygame.image.load(corps).convert_alpha()
-        for i in range(1, snake.nb_carres):
-            image_corps = pygame.transform.scale(image_corps, (snake.carres[i].taille, snake.carres[i].taille))
-            fenetre.blit(image_corps, (snake.carres[i].x, snake.carres[i].y))
-        fenetre.blit(image_pomme, (pomme.x, pomme.y))
-
+            # prev New
+        if snake.estMort == False:
+            for i in range(1, snake.nb_carres):
+                image_corps = pygame.transform.scale(image_corps, (snake.carres[i].taille, snake.carres[i].taille))
+                fenetre.blit(image_corps, (snake.carres[i].x, snake.carres[i].y))
+            fenetre.blit(image_pomme, (pomme.x, pomme.y))
+        if snake.estMort == True:
+            time.sleep(1)
         pygame.display.flip()
+        # ENew
 
     while game_over:
-        pygame.time.Clock().tick(FPS)
+        pygame.time.Clock().tick(30)
 
         texte_score = score.afficher()
         texte_score_rect = texte_score.get_rect()
