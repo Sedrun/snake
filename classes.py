413bacb0496c6966d1e0141411a71411698b4836
Sedrun <ili-dan@hotmail.fr>
2020-01-15 19:58:56 +0100

    update1

--- classes.py
+++ classes.py
@@ -10,8 +10,8 @@ class Carre:
         self.y = y
         self.taille = taille
 
-class Pomme(Carre):
-    def __init__(self, image, taille): #TODO: à refaire
+class Pomme:
+    def __init__(self, image, taille):
         self.case_x = rand.randint(0, nombre_sprite_cote-1)
         self.case_y = rand.randint(0, nombre_sprite_cote-1)
         self.x = self.case_x * taille_sprite
@@ -25,7 +25,6 @@ class Pomme(Carre):
         self.y = self.case_y * taille_sprite
 
 class Snake:
-    #NEW
     def __init__(self, image, case_x, case_y):
         #position du personnage en case et en pixel
         self.case_x = case_x
@@ -35,21 +34,22 @@ class Snake:
         self.image = image
         #carres
         self.tete = Carre(self.image, self.case_x, self.case_y, taille)
-        self.carres = [self.tete]
-        self.nb_carres = 1
+        #NEW
+        self.carres = [self.tete,Carre(self.image, self.x-taille_sprite, self.y, taille),Carre(self.image, self.x-2*taille_sprite, self.y, taille)]
+        self.nb_carres = 3
+        #ENEW
         #direction
         self.direction = "droite"
         #est-il mort ?
         self.estMort = False
-    #ENEW
     def addCarre(self):
-        self.carres.append(Carre(self.image, self.x, self.y, taille)) #NEW
+        self.carres.append(Carre(self.image, self.x, self.y, taille))
         self.nb_carres += 1
 
     def initialisation(self):
         for i in range (self.nb_carres-1, 0, -1):
             self.carres.pop(i)
-        self.nb_carres = 1
+        #new
         self.estMort = False
 
     def deplacer(self, direction):
@@ -66,30 +66,45 @@ class Snake:
                 self.x = self.case_x * taille_sprite
             else: self.estMort = True
         #Déplacement vers la gauche
-        elif direction == 'gauche':
+        if direction == 'gauche':
             if self.case_x > 0:
                 self.case_x -= 1
                 self.x = self.case_x * taille_sprite
-            else: self.estMort = True
+            else:
+                self.estMort = True
 
         # Déplacement vers le haut
-        elif direction == 'haut':
+        if direction == 'haut':
             if self.case_y > 0:
                 self.case_y -= 1
                 self.y = self.case_y * taille_sprite
-            else: self.estMort = True
+            else:
+                self.estMort = True
 
         # Déplacement vers le bas
-        elif direction == 'bas':
+        if direction == 'bas':
             if self.case_y < (nombre_sprite_cote - 1):
                 self.case_y += 1
                 self.y = self.case_y * taille_sprite
-            else: self.estMort = True
+            else:
+                self.estMort = True
         #permet le déplacement de chaque partie du corps
-        for i in range(self.nb_carres -1, 0, -1):
-            self.carres[i].x = self.carres[i-1].x
-            self.carres[i].y = self.carres[i-1].y
-#New
+        if self.estMort == False:
+            for i in range(self.nb_carres - 1, 0, -1):
+                self.carres[i].x = self.carres[i-1].x
+                self.carres[i].y = self.carres[i-1].y
+    # New
+    def accueil(self):
+        if self.case_y == 0 and self.case_x < 19:
+            self.deplacer('droite')
+        if self.case_x == 19 and self.case_y < 19:
+            self.deplacer('bas')
+        if self.case_y == 19 and self.case_x > 0:
+            self.deplacer('gauche')
+        if self.case_x == 0 and self.case_y > 0:
+            self.deplacer('haut')
+    # ENew
+
 class Score:
     def __init__(self):
         self.total = 0
@@ -107,5 +122,3 @@ class Score:
 
     def afficher(self):
         return fontH2.render("score : " + str(self.total), 1, (255, 255, 255))  # récupération du score en string
-
-#ENew
